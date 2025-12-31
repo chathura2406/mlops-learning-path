bc69783d3063bd8e990d5be13c23efa266537293
chathura2406 <it23689480@my.sliit.lk>
2025-12-31 07:14:17 +0530

    Day 1: Updated system to read data from CSV file

--- student_system.py
+++ student_system.py
@@ -1,24 +1,46 @@
-class Student: 
-    def __init__(self, name , age,marks ):
+# 1. Api kalin hadapu Class eka
+class Student:
+    def __init__(self, name, age, marks):
         self.name = name
-        self.age =age
-        self.marks = marks
+        self.age = age
+        self.marks = int(marks) # Data enne text widiyata nisa, Marks number ekak karaganna oni (int)
 
+    def get_grade(self):
+        # Grade eka return karamu print karanne nathuwa
+        if self.marks > 75:
+            return "A"
+        elif self.marks > 50:
+            return "B"
+        else:
+            return "C"
 
-    def get_grade (self):
-        if self.marks >= 80:
-            print(f"{self.name} has received an A Grade ")
-        elif self .marks >= 60:
-            print (f"{self.name} has received teh B Pass")
-        else :
-            print ("cant determined the grade")
+# --- Main Part (Real World Logic) ---
 
+print("Processing Student Data...\n")
 
-student1 =Student ("sadun ", 19, 85 )
-student2 =Student ("kumar",20,67)
+# 2. File eka open karaganna
+# 'r' kiyanne Read mode (Kiyawanna witharai)
+file = open("students.csv", "r")
 
+# 3. Hama line ekakama kiyawanna (Loop)
+for line in file:
+    # Line eka: "Kamal,21,85\n"
+    
+    # strip() -> Line eke agata thiyena spaces ain karanawa
+    # split(",") -> "," thiyena than walin kadanawa -> ["Kamal", "21", "85"]
+    data = line.strip().split(",")
+    
+    name = data[0]
+    age = data[1]
+    marks = data[2]
 
-student1.get_grade ()
-student2.get_grade ()
+    # 4. Object eka hadanawa
+    student_obj = Student(name, age, marks)
+    
+    # 5. Result eka gannawa
+    grade = student_obj.get_grade()
+    
+    print(f"{student_obj.name} ge Grade eka: {grade}")
 
-
+# File eka close krnna  (Memory save karanna)
+file.close()
